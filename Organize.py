803840705fbc712070d26363c77c775f6e2d1e32
jaronoff97 <jacobaronoff45@gmail.com>
2016-05-01 22:50:56 -0400

    added ability to show all keys

--- Organize.py
+++ Organize.py
@@ -2,7 +2,6 @@ import os.path
 import datetime
 import getpass
 import os
-import operator
 import shutil
 from TypeOfOrg import TypeOfOrg
 from Accepted_Files import accepted_files
@@ -11,15 +10,19 @@ import DictionaryHelper as dictionary
 
 class Organize(object):
     """docstring for Organize"""
-    baseDir = endDir = '/Users/' + getpass.getuser() + '/'
+    baseDir = '/Users/' + getpass.getuser() + '/'
     endDir = '/Users/' + getpass.getuser() + '/Documents/Organized_Downloads/'
 
-    def __init__(self, folder, organization_method=TypeOfOrg.Month):
+    def __init__(self, folder,
+                 organization_method=TypeOfOrg.Month,
+                 show_all_keys=False):
         super(Organize, self).__init__()
         if not os.path.exists(Organize.endDir):
             os.makedirs(Organize.endDir)
         self.folder = Organize.baseDir + folder + '/'
         self.organization_method = organization_method
+        self.all_keys = {}
+        self.show_all_keys = show_all_keys
 
     def organize(self):
         for filename in os.listdir(self.folder):
@@ -28,6 +31,8 @@ class Organize(object):
                     self.moveFileByTime(filename)
                 elif self.organization_method is TypeOfOrg.Filename:
                     self.moveFileByName(filename)
+        if self.show_all_keys:
+            print(self.all_keys)
 
     def findModifiedTime(self, filename):
         theTime = os.path.getmtime(filename)
@@ -35,15 +40,17 @@ class Organize(object):
             TypeOfOrg.modifiedTimeString(self.organization_method))
 
     def moveFileByTime(self, filename):
-        modified = self.findModifiedTime(self.folder + filename)
+        final_path = self.folder + filename
+        modified = self.findModifiedTime(final_path)
         if not os.path.exists(Organize.endDir + modified):
             os.makedirs(Organize.endDir + modified)
         self.moveAndRemove(
-            self.folder + filename, Organize.endDir +
+            final_path, Organize.endDir +
             modified + '/' + filename)
 
     def moveFileByName(self, filename):
         moved = False
+        final_path = self.folder + filename
         for key, values in accepted_files.items():
             if not os.path.exists(Organize.endDir + key):
                 os.makedirs(Organize.endDir + key)
@@ -52,15 +59,15 @@ class Organize(object):
             for value in values:
                 try:
                     if value in filename.lower() and os.path.isfile(
-                            self.folder + filename):
+                            final_path):
                         self.moveAndRemove(
-                            self.folder + filename, Organize.endDir +
+                            final_path, Organize.endDir +
                             key + '/' + filename)
                         moved = True
                 except Exception as e:
                     print("ERROR {0}".format(e))
         if not moved:
-            contents = self.countContents(self.folder + filename)
+            contents = self.countContents(final_path)
             common_key = []
             if not contents:
                 pass
@@ -69,10 +76,10 @@ class Organize(object):
                 common_key = dictionary.getKeyFromValue(
                     accepted_files, max_val)
             if common_key:
-                self.moveAndRemove(self.folder + filename, Organize.endDir +
+                self.moveAndRemove(final_path, Organize.endDir +
                                    common_key + '/' + filename)
             else:
-                self.moveAndRemove(self.folder + filename, Organize.endDir +
+                self.moveAndRemove(final_path, Organize.endDir +
                                    'Other' + '/' + filename)
 
     def moveAndRemove(self, filename, destination):
@@ -93,12 +100,20 @@ class Organize(object):
         if os.path.isdir(folder):
             for file in os.listdir(folder):
                 key = os.path.splitext(file)[1]
-                if os.path.isdir(folder + '/' + file):
+                if (os.path.isdir(folder + '/' + file) and
+                        not os.path.isfile(folder + '/' + file)):
+                    internal_files = self.countContents(folder + '/' + file)
                     file_types = dictionary.merge(
-                        file_types, self.countContents(folder + '/' + file))
+                        file_types.copy(),
+                        internal_files)
                 else:
                     if dictionary.value_exists(file_types, key):
                         file_types[key] = file_types.get(key, 0) + 1
                     else:
                         file_types[key] = 1
+                    if self.show_all_keys:
+                        if dictionary.value_exists(self.all_keys, key):
+                            self.all_keys[key] = self.all_keys.get(key, 0) + 1
+                        else:
+                            self.all_keys[key] = 1
         return file_types
